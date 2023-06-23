daa801081117e755e4647146b591971f96a3ec8e
2424nrr <137486579+2424nrr@users.noreply.github.com>
2023-06-23 16:50:06 +0900

    Create discordbot.py

---
+++ discordbot.py
@@ -0,0 +1,13 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+print('hi!')
+
+@client.event
+if message.content == '/hello:
+await message.channel.send('fuck!')
+
+client.run("MTEyMTcwMTEzOTQwMjk4OTY4OA.G5PP9q.q3lKWmoJqNJXsrskceVoQJecBaV2RGP4mHsc5o")
